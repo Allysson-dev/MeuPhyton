274d5dcbc39079a8f28a3407ade9908dd5928ea3
Allysson-dev <147248214+Allysson-dev@users.noreply.github.com>
2023-10-27 20:23:30 -0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,21 @@
+from random import randint
+
+numero = randint(1, 100)
+tentativas = 0
+
+print("Bem-vindo ao jogo da adivinhação")
+print("Eu escolhi um número entre 1 e 100. Você tem 10 tentativas para adivinhar.")
+
+while tentativas < 10:
+    palpite = int(input("Digite o seu palpite: "))
+    tentativas += 1
+
+    if palpite == numero:
+        print(f"Parabéns! Você acertou o número em {tentativas} tentativas.")
+        break
+    elif palpite < numero:
+        print("O número é maior. Tente novamente.")
+    else:
+        print("O número é menor. Tente novamente.")
+else:
+    print(f"Você perder! O número era {numero}.")
